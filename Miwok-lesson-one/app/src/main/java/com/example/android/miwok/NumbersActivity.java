0bfb3ade0d3a29d2fc52bcf31b2d8bb49147ad00
gigwegbe <gigwegbe@gmail.com>
2017-09-26 19:01:57 +0100

    use of ArrayList

--- Miwok-lesson-one/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok-lesson-one/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -40,5 +43,13 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        TextView wordView = new TextView(this);
+        wordView.setText(words.get(0));
+        wordView.setText(words.get(1));
+
+
+
     }
 }
